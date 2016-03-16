79a217560056b47958e28a0d8c19f45c845fbd3b
k2346 <k2346@student.jamk.fi>
2016-03-16 10:56:02 +0200

    Tunnilla tehty esimerkki sivuilla siirtymisestä

---
+++ multiplePagesUp/GamePage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace multiplePagesUp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class GamePage : Page
+    {
+        public GamePage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            //peli on pelattu ja Score on tiedossa
+            int score = 100;
+            //sijoitetaan pelin tulos pääohjelmassa olevaan Score ominaisuuteen
+            (App.Current as App).Score = score;
+            // get root frame (which show pages) Navigoidaan takaisin pääsivulle 
+            Frame rootFrame = Window.Current.Content as Frame;
+            // did we get it correctly
+            if (rootFrame == null) return;
+            // navigate back if possible 
+            if (rootFrame.CanGoBack)
+            {
+                rootFrame.GoBack();
+            }
+        }
+        //sivulle on tultu
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter is string)
+            {
+               
+                GreetingTextBlock.Text = "Hei sinä ihana, " + e.Parameter;
+            }
+          
+            base.OnNavigatedTo(e);
+        }
+    }
+}
---
+++ multiplePagesUp/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace multiplePagesUp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            string firstname = NameTextBox.Text;
+            this.Frame.Navigate(typeof(GamePage),firstname);
+
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Debug.WriteLine ("Score is: " + (App.Current as App).Score);
+            base.OnNavigatedTo(e);
+        }
+    }
+}
